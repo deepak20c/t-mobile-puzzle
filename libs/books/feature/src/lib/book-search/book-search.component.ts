c23f8c3014ebdf7095e86192ac3e1187bea50827
Deepak Christhuraj <deepak.christhuraj75@t-mobile.com>
2024-01-22 19:17:39 -0500

    Task 3: Undo add and remove from reading list

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -5,10 +5,12 @@ import {
   clearSearch,
   getAllBooks,
   ReadingListBook,
-  searchBooks
+  searchBooks,
+  removeFromReadingList
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'tmo-book-search',
@@ -24,7 +26,8 @@ export class BookSearchComponent implements OnInit {
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder
+    private readonly fb: FormBuilder,
+    private snackBar: MatSnackBar
   ) {}
 
   get searchTerm(): string {
@@ -45,6 +48,18 @@ export class BookSearchComponent implements OnInit {
 
   addBookToReadingList(book: Book) {
     this.store.dispatch(addToReadingList({ book }));
+    this.undoAddToReadingList(book);
+  }
+
+  undoAddToReadingList(book: Book) {
+    const snackBarRef = this.snackBar
+      .open(`${book.title} added to the reading list`, 'Undo', { duration: 3000 });
+
+    snackBarRef.onAction().subscribe(() => {
+      this.store.dispatch(removeFromReadingList({
+        item: { bookId: book.id, ...book },
+      })
+    )});
   }
 
   searchExample() {
